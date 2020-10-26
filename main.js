861319e0a5723b299ac4a2517bfd0d37c06cf3e7
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-26 14:19:27 +0100

    Inizio esercizio

---
+++ main.js
@@ -0,0 +1,10 @@
+// il codice già predisposto di una navbar con hamburger menu su mobile. Quello che troverete nella repo sono infatti:
+// file html già pronto, con la struttura per la navbar, che su mobile diventa hamburger
+// file css già pronto, con tutte le media query necessarie per trasformare la navbar in hamburger menu
+// file js vuoto
+// Dovete quindi analizzare la struttura e il css utilizzati e predisposti nella repo per integrare la vostra logica nel file js. Al click sull'icona dell'hamburger menu, deve essere visualizzato il menu mobile e al click sull'icona "X" il menu mobile deve scomparire.
+// In altre parole, dovere scrivere solo il codice js necessario a far funzionare l'hamburger menu.
+
+// click icona hamburger menu -> si deve visualizzare il menu mobile
+
+// click su icona X -> menu mobile scompare
