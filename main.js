69f6c61fc69170e7cd0e64600e68f38b785db591
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-26 15:06:14 +0100

    fine esercizio

--- main.js
+++ main.js
@@ -5,6 +5,14 @@
 // Dovete quindi analizzare la struttura e il css utilizzati e predisposti nella repo per integrare la vostra logica nel file js. Al click sull'icona dell'hamburger menu, deve essere visualizzato il menu mobile e al click sull'icona "X" il menu mobile deve scomparire.
 // In altre parole, dovere scrivere solo il codice js necessario a far funzionare l'hamburger menu.
 
-// click icona hamburger menu -> si deve visualizzare il menu mobile
 
-// click su icona X -> menu mobile scompare
+// click icona hamburger menu -> si deve visualizzare il menu mobile: trovare selettori
+// NB: ricordare di inserire il "."
+
+$('.fa-bars').click(function() {
+    $('.hamburger-menu').show();
+});
+// click su icona X -> menu mobile scompare: trovare selettori
+$('.fa-times').click(function() {
+    $('.hamburger-menu').hide();
+});
